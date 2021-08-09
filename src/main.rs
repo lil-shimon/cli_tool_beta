9ac98ccccdbeeb266e5bffb300334971625d6309
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-09 20:35:46 +0900

    ls

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,24 @@ fn main() {
         .output()
         .expect("failed to excute this command");
 
+
     let echo_hello = output.stdout;
 
     println!("{}", std::str::from_utf8(&echo_hello).unwrap());
 
+    ls_command();
+
 }
 
+
+fn ls_command () {
+    let ls_output = Command::new("sh")
+        .arg("-c")
+        .arg("ls -a")
+        .output()
+        .expect("error");
+
+    let ls_command = ls_output.stdout;
+
+    println!("{:?}", std::str::from_utf8(&ls_command));
+}
